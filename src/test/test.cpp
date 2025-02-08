5b5d85b7fa89c40ec2f98c8e846c547ee3025dbb
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2025-02-09 00:52:08 +0100

    testing XOR gate

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -39,3 +39,48 @@ TEST_CASE("Perceptron AND gate", "[perceptron]")
     double loss = andGate.loss(inputs, targets);
     std::cout << "Loss: " << loss << std::endl;
 }
+
+TEST_CASE("Perceptron XOR gate", "[Perceptron XOR]")
+{
+    Perceptron xorGate(WEIGHTS, BIAS, LEARNING_RATE);
+    std::vector<std::vector<int>> inputs = {in00, in01, in10, in11};
+    std::vector<int> targets = {0, 1, 1, 0};
+
+    // Train the perceptron
+    xorGate.update(inputs, targets, 100);
+
+    CHECK(xorGate.predict(in00) == 0);
+    CHECK(xorGate.predict(in01) == 1);
+    CHECK(xorGate.predict(in10) == 1);
+    CHECK(xorGate.predict(in11) == 0);
+
+    // Print the weights
+    xorGate.__str__(1);
+
+    // Calculate the loss
+    double loss = xorGate.loss(inputs, targets);
+    std::cout << "Loss: " << loss << std::endl;
+}
+
+
+// Perceptron XOR gate
+// -------------------------------------------------------------------------------
+
+// FAILED:
+//   CHECK( xorGate.predict(in00) == 0 )
+// with expansion:
+//   1 == 0
+
+// FAILED:
+//   CHECK( xorGate.predict(in10) == 1 )
+// with expansion:
+//   0 == 1
+
+// weights:
+// -0.1 2.77556e-17 
+// bias = 2.77556e-17
+// Learning rate = 0.1
+// Loss: 0.5
+
+// A XOR gate is a linearly inseparable function, which means that a single perceptron
+// cannot learn the weights to create a XOR gate.
