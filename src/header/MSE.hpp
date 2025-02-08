93aaba3b9e61a72f1135762dbda3b35bcb0d224f
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2025-02-08 22:47:31 +0100

    MSE docs

--- src/header/MSE.hpp
+++ src/header/MSE.hpp
@@ -5,7 +5,7 @@
 /**
  * @brief Calculates the mean squared error between two vectors.
  * MSE = Σ | d – y |2 / n
- * 
+ *  
  * This function computes the mean squared error (MSE) between the target values
  * and the predicted values. The MSE is a measure of the average squared difference
  * between the estimated values and the actual value.
@@ -15,6 +15,7 @@
  * @return The mean squared error between the targets and predictions. 
  *         Returns -1 if the sizes of the input vectors do not match.
  */
+
 double mean_squared_error(const std::vector<double>& targets, const std::vector<double>& predictions) 
 {   
     // Ensure both arrays are the same size
