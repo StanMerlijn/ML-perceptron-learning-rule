7adb842733630d68bb5d6be2a8a030834a4780dd
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2025-02-08 22:32:15 +0100

    reorder files

---
+++ src/test/test.cpp
@@ -0,0 +1,17 @@
+#include "../header/perceptron.hpp"
+#include "../header/MSE.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <iostream>
+#include <vector>
+
+TEST_CASE("Perceptron class", "[perceptron]")
+{
+    CHECK(1 == 1);
+}
+
+
+
+
